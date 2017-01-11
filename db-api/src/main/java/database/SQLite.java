547f6c99f102c083a9332559dded3ca4f1e51934
Florian Thiery <web@florian-thiery.de>
2017-01-11 08:51:01 +0100

    forms functionallity changed

--- db-api/src/main/java/database/SQLite.java
+++ db-api/src/main/java/database/SQLite.java
@@ -18,6 +18,7 @@ public class SQLite {
 
     private static final String DBDRIVER = "org.sqlite.JDBC";
     private static final String DBFILE = "C:/tmp/caa-hd.sqlite";
+    private static final String IMGFOLDER = "C:/tmp/forms/";
     private static final String LSHOST = "http://143.93.114.135";
 
     public static JSONObject getForm(String id) throws SQLException, ClassNotFoundException, IOException {
@@ -28,10 +29,14 @@ public class SQLite {
                 String sql = "SELECT * FROM form WHERE id = " + id;
                 try (ResultSet rs = stmt.executeQuery(sql)) {
                     while (rs.next()) {
-                        form.put("id", Integer.parseInt(rs.getString("id")));
-                        form.put("img", rs.getString("img"));
+                        if (rs.getString("img").contains("null")) {
+                            form.put("img", "");
+                        } else {
+                            form.put("img", IMGFOLDER + rs.getString("img"));
+                        }
+                        form.put("localname", rs.getString("localname"));
                         String ls = rs.getString("ls");
-                        form.put("ls", ls);
+                        form.put("concept", ls);
                         String[] lsSplit = ls.split("/");
                         String lsID = lsSplit[lsSplit.length - 1];
                         String url_string = LSHOST + "/api/v1/labels/" + lsID + "?equalConcepts=true";
@@ -48,20 +53,45 @@ public class SQLite {
                         br.close();
                         JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.toString());
                         JSONArray equalConcepts = (JSONArray) jsonObject.get("equalConcepts");
-                        //form.put("ls-entry", jsonObject);
-                        form.put("name", jsonObject.get("thumbnail"));
+                        form.put("label", jsonObject.get("thumbnail"));
+                        if (jsonObject.get("description") != null) {
+                            form.put("description", jsonObject.get("description"));
+                        } else {
+                            form.put("description", "");
+                        }
                         JSONArray equalNames = new JSONArray();
                         Map<String, String> map = new HashMap<String, String>();
                         for (Object item : equalConcepts) {
                             JSONObject obj = (JSONObject) item;
-                            map.put(obj.get("id").toString(), obj.get("thumbnail").toString());
+                            map.put(obj.get("id").toString(), obj.get("thumbnail").toString() + ";" + form.get("description"));
                         }
                         Iterator<Entry<String, String>> iterator = map.entrySet().iterator();
                         while (iterator.hasNext()) {
                             Map.Entry<String, String> entry = (Map.Entry<String, String>) iterator.next();
                             JSONObject obj = new JSONObject();
-                            obj.put("uri", LSHOST + "/item/label/" + entry.getKey());
-                            obj.put("name", entry.getValue());
+                            String[] valuesplit = entry.getValue().split(";");
+                            obj.put("concept", LSHOST + "/item/label/" + entry.getKey());
+                            if (valuesplit.length == 1) {
+                                obj.put("label", valuesplit[0]);
+                                obj.put("description", "");
+                            } else {
+                                obj.put("label", valuesplit[0]);
+                                obj.put("description", valuesplit[1]);
+                            }
+                            // get data from database
+                            String sql2 = "SELECT * FROM form WHERE ls LIKE \"%" + entry.getKey() + "%\"";
+                            try (ResultSet rs2 = stmt.executeQuery(sql2)) {
+                                while (rs2.next()) {
+                                    if (rs.getString("img").contains("null")) {
+                                        obj.put("img", "");
+                                    } else {
+                                        obj.put("img", IMGFOLDER + rs.getString("img"));
+                                    }
+                                    obj.put("localname", rs.getString("localname"));
+                                }
+                            } catch (Exception e) {
+                                throw new NullPointerException("cannot get form. " + e.toString());
+                            }
                             equalNames.add(obj);
                         }
                         form.put("equals", equalNames);
@@ -83,43 +113,13 @@ public class SQLite {
                 try (ResultSet rs = stmt.executeQuery(sql)) {
                     while (rs.next()) {
                         JSONObject form = new JSONObject();
-                        form.put("id", Integer.parseInt(rs.getString("id")));
-                        form.put("img", rs.getString("img"));
-                        String ls = rs.getString("ls");
-                        form.put("ls", ls);
-                        String[] lsSplit = ls.split("/");
-                        String lsID = lsSplit[lsSplit.length - 1];
-                        String url_string = LSHOST + "/api/v1/labels/" + lsID + "?equalConcepts=true";
-                        URL url = new URL(url_string);
-                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                        conn.setRequestProperty("Accept", "application/json");
-                        conn.setRequestProperty("Accept-Encoding", "*");
-                        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-                        String inputLine;
-                        StringBuilder response = new StringBuilder();
-                        while ((inputLine = br.readLine()) != null) {
-                            response.append(inputLine);
+                        if (rs.getString("img").contains("null")) {
+                            form.put("img", "");
+                        } else {
+                            form.put("img", IMGFOLDER + rs.getString("img"));
                         }
-                        br.close();
-                        JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.toString());
-                        JSONArray equalConcepts = (JSONArray) jsonObject.get("equalConcepts");
-                        //form.put("ls-entry", jsonObject);
-                        form.put("name", jsonObject.get("thumbnail"));
-                        JSONArray equalNames = new JSONArray();
-                        Map<String, String> map = new HashMap<String, String>();
-                        for (Object item : equalConcepts) {
-                            JSONObject obj = (JSONObject) item;
-                            map.put(obj.get("id").toString(), obj.get("thumbnail").toString());
-                        }
-                        Iterator<Entry<String, String>> iterator = map.entrySet().iterator();
-                        while (iterator.hasNext()) {
-                            Map.Entry<String, String> entry = (Map.Entry<String, String>) iterator.next();
-                            JSONObject obj = new JSONObject();
-                            obj.put("uri", LSHOST + "/item/label/" + entry.getKey());
-                            obj.put("name", entry.getValue());
-                            equalNames.add(obj);
-                        }
-                        form.put("equals", equalNames);
+                        form.put("localname", rs.getString("localname"));
+                        form.put("concept", rs.getString("ls"));
                         forms.add(form);
                     }
                 }
@@ -129,5 +129,4 @@ public class SQLite {
         }
         return forms;
     }
-
 }
--- db-api/src/main/java/rest/GenericResource.java
+++ db-api/src/main/java/rest/GenericResource.java
@@ -7,7 +7,6 @@ import utils.Logging;
 import java.io.IOException;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
